cbe6814a1c0f926d9c418067482d0674900a4e8e
indoriarohan00-cpu <indoriarohan00@gmail.com>
2025-11-16 21:58:29 +0530

    Add files via upload

---
+++ Leetcode1672.java
@@ -0,0 +1,17 @@
+public class Leetcode1672 {
+    public static void main(String[] args){
+
+    }
+    public int maximumWealth(int[][] accounts) {
+        int ans = Integer.MIN_VALUE;
+    for(int person =0; person < accounts.length; person++){
+        int sum =0;
+
+        for(int account =0; account< accounts[person].length; account++){
+            sum = sum + person[person][account]
+
+        }
+        int max = 0;
+    }    
+    }
+}
